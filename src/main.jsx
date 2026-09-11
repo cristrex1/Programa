fa8313f4af38c9681f42f2635a9ca4f3c9e6c000
cristrex1 <scalzonecristian@gmail.com>
2026-09-10 23:22:08 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Tienda from "./Tienda.jsx";
 
-const esTienda = window.location.pathname.startsWith("/tienda");
+const esAdmin = window.location.pathname.startsWith("/admin");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {esTienda ? <Tienda /> : <App />}
+    {esAdmin ? <App /> : <Tienda />}
   </React.StrictMode>
 );
